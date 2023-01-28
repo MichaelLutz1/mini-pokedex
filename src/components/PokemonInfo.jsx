a4fd942dc85977917c7070c0dd1795dabedad58e
Michael Lutz <mikelutz2400@gmail.com>
2023-01-27 19:52:49 -0500

    error message if pokemon doesn't exist

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./Searchbar";
 
 function PokemonInfo() {
   const [pokemonName, setPokemonName] = useState("");
+  const [pokemonData, setPokemonData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("invisible");
+  useEffect(() => {
+    async function fetchPokemon(pokemonName) {
+      try {
+        setIsLoading(true);
+        const pokemonResponse = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+        );
+        const pokemon = await pokemonResponse.json();
+        console.log(pokemon);
+        setPokemonData(pokemon);
+        setPokemonName(pokemon.name);
+        setIsLoading(false);
+      } catch {
+        setError("visible");
+        setIsLoading(false);
+        setPokemonName("");
+        setTimeout(() => {
+          setError("invisible");
+        }, 3000);
+      }
+    }
+    pokemonName === "" ? fetchPokemon("pikachu") : fetchPokemon(pokemonName);
+  }, [pokemonName]);
   return (
     <>
-      <SearchBar setPokemonName={setPokemonName} />
+      <SearchBar setPokemonName={setPokemonName} error={error} />
       <div>{pokemonName}</div>
+      <pre>{JSON.stringify(pokemonData, null, 2)}</pre>
     </>
   );
 }
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 function SearchBar(props) {
-  const { register, handleSubmit } = useForm();
-  function onSubmit(input) {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (input) => {
     props.setPokemonName(input.pokemon);
-  }
+    reset();
+  };
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Enter a pokemon:{" "}
-          <input {...register("pokemon", { required: true })} type="search" />
-        </label>
+        <input
+          {...register("pokemon", { required: true })}
+          autoComplete="off"
+          placeholder="Enter a Pokemon"
+        />
         <input type="submit" />
       </form>
+      <div className={props.error}>error: pokemon does not exist</div>
     </>
   );
 }
